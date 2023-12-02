4396fbf2015e1c722d36bb7d6213ef14d5f64be4
Witut2005 <wiktor.glogowski@onet.pl>
2023-12-02 19:37:38 +0100

    project new folder tree

---
+++ frontend/src/app/transact/transact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transact',
+  standalone: true,
+  imports: [],
+  templateUrl: './transact.component.html',
+  styleUrl: './transact.component.css'
+})
+export class TransactComponent {
+
+}
